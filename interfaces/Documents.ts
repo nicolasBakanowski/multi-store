54786c9ba03c0bf286d77aa0aab24527fb1bb7ac
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-08-08 22:11:07 -0300

    chore: delete useless files

--- interfaces/Documents.ts
+++
@@ -1,3 +0,0 @@
-export interface FormCreateDocumentProps {
-  folder_id?: string;
-}
--- pages/home.tsx
+++
@@ -1,7 +0,0 @@
-import SideBarMenu from "../components/sideBarMenu";
-
-const HomePage = () => {
-  return <div></div>;
-};
-
-export default HomePage;
