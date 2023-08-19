6e2d3f184838d5be6d450989a8017682dc7f98f8
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-08-19 04:37:52 -0300

    feat: add domain images in next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["localhost"], // Agrega el dominio "localhost" aquí
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
