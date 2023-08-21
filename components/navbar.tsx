619274558b004f8365b09b9f25ab8c9b95c19b3e
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-08-21 05:18:09 -0300

    feat: add back botton  and logic

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,9 +3,11 @@ import NavbarIcon from "../public/hamburger.svg";
 import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
 import CartIcon from "../public/cart.svg";
-import HomeIcon from "../public/home.svg"; // Import your cart icon image
+import HomeIcon from "../public/home.svg";
+import BackIcon from "../public/back.svg";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -19,6 +21,9 @@ const Navbar = () => {
       setMenuOpen(false);
     }
   };
+  const handleGoBack = () => {
+    router.back();
+  };
 
   useEffect(() => {
     window.addEventListener("resize", closeMenuOnResize);
@@ -26,54 +31,73 @@ const Navbar = () => {
       window.removeEventListener("resize", closeMenuOnResize);
     };
   }, []);
-
+  const router = useRouter();
+  const isIndexPage = router.pathname === "/";
   return (
-    <nav className="bg-black text-white py-4 px-6 flex justify-between items-center">
-      <div className={`${menuOpen ? "hidden" : "md:block"} text-2xl font-bold`}>
-        Market
-      </div>
-      <ul
-        className={`${
-          menuOpen ? "block" : "hidden"
-        } md:flex md:space-x-4 md:justify-center mt-4 md:mt-0`}
-      >
-        <li>
-          <Link href="/" className="block md:inline">
-            <Image
-              src={HomeIcon}
-              alt="Cart Icon"
-              className="h-7 w-7 bottom-10"
-            />
-          </Link>
-        </li>
-        <li>
-          <Link href="/cart" className="block md:inline">
-            <div className="relative">
+    <div>
+      <nav className="bg-black text-white py-4 px-6 flex justify-between items-center">
+        <div
+          className={`${menuOpen ? "hidden" : "md:block"} text-2xl font-bold`}
+        >
+          Market
+        </div>
+        <ul
+          className={`${
+            menuOpen ? "block" : "hidden"
+          } md:flex md:space-x-4 md:justify-center mt-4 md:mt-0`}
+        >
+          <li>
+            <Link href="/" className="block md:inline">
               <Image
-                src={CartIcon}
-                alt="Cart Icon"
-                className=" top-4 h-8 w-8"
+                src={HomeIcon}
+                alt="Home Icon"
+                className="h-7 w-7 bottom-10"
               />
-              {cartItems > 0 && (
-                <span className="absolute top-4 left-4  bg-red-500 text-white rounded-full p-0.5 text-xs">
-                  {cartItems}
-                </span>
-              )}
-            </div>
-          </Link>
-        </li>
-      </ul>
-      <button
-        className="block md:hidden text-white focus:outline-none"
-        onClick={toggleMenu}
-      >
-        {menuOpen ? (
-          <Image src={NavbarIcon} alt="Close Icon" className="h-7 w-7" />
-        ) : (
-          <Image src={NavbarIcon} alt="Menu Icon" className="h-7 w-7" />
+            </Link>
+          </li>
+          <li>
+            <Link href="/cart" className="block md:inline">
+              <div className="relative">
+                <Image
+                  src={CartIcon}
+                  alt="Cart Icon"
+                  className="top-4 h-8 w-8"
+                />
+                {cartItems > 0 && (
+                  <span className="absolute top-4 left-4 bg-red-500 text-white rounded-full p-0.5 text-xs">
+                    {cartItems}
+                  </span>
+                )}
+              </div>
+            </Link>
+          </li>
+        </ul>
+        <button
+          className="block md:hidden text-white focus:outline-none"
+          onClick={toggleMenu}
+        >
+          {menuOpen ? (
+            <Image src={NavbarIcon} alt="Close Icon" className="h-7 w-7" />
+          ) : (
+            <Image src={NavbarIcon} alt="Menu Icon" className="h-7 w-7" />
+          )}
+        </button>
+        {!isIndexPage && (
+          <div
+            className={`absolute ${
+              menuOpen ? "top-32 md:top-20" : "top-20"
+            } left-8`}
+          >
+            <button onClick={handleGoBack}>
+              <div className="flex items-center space-x-1">
+                <Image src={BackIcon} alt="Back Icon" className="h-6 w-6" />
+                <span>Volver</span>
+              </div>
+            </button>
+          </div>
         )}
-      </button>
-    </nav>
+      </nav>
+    </div>
   );
 };
 
--- pages/category/[categoryId].tsx
+++ pages/category/[categoryId].tsx
@@ -13,42 +13,15 @@ const CategoryPage = () => {
     (state: RootState) => state.category.currentCategory
   );
   const products = useSelector((state: RootState) => state.product.products);
-  const handleGoBack = () => {
-    router.back();
-  };
+
   useEffect(() => {
     if (currentCategory) {
       dispatch(fetchProductsByCategory(currentCategory.id) as any);
     }
   }, [currentCategory, dispatch]);
-  console.log("estos deberian ser los productos en el ", products);
-
   return (
     <div>
       <main className="container mx-auto mt-10 p-4">
-        <div className="mb-4">
-          <button
-            onClick={handleGoBack}
-            className="flex items-center text-gray-600 hover:text-gray-800"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="h-5 w-5 mr-2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M15 19l-7-7 7-7"
-              />
-            </svg>
-            Volver al Inicio
-          </button>
-        </div>
-        <h1 className="text-3xl font-semibold mb-4">Category Name</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {products.map((product: Product) => (
             <ProductCard key={product.id} product={product} />
