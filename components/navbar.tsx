f8f96413b0907134a1a8fce353b700ef9f15f265
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-08-22 20:48:55 -0300

    feat: add login and change icon in nav

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,6 +5,7 @@ import { RootState } from "@/redux/store";
 import CartIcon from "../public/cart.svg";
 import HomeIcon from "../public/home.svg";
 import BackIcon from "../public/back.svg";
+import loginIcon from "../public/login.svg";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -21,6 +22,7 @@ const Navbar = () => {
       setMenuOpen(false);
     }
   };
+
   const handleGoBack = () => {
     router.back();
   };
@@ -31,8 +33,10 @@ const Navbar = () => {
       window.removeEventListener("resize", closeMenuOnResize);
     };
   }, []);
+
   const router = useRouter();
   const isIndexPage = router.pathname === "/";
+
   return (
     <div>
       <nav className="bg-black text-white py-4 px-6 flex justify-between items-center">
@@ -41,61 +45,72 @@ const Navbar = () => {
         >
           Market
         </div>
-        <ul
-          className={`${
-            menuOpen ? "block" : "hidden"
-          } md:flex md:space-x-4 md:justify-center mt-4 md:mt-0`}
-        >
-          <li>
-            <Link href="/" className="block md:inline">
-              <Image
-                src={HomeIcon}
-                alt="Home Icon"
-                className="h-7 w-7 bottom-10"
-              />
-            </Link>
-          </li>
-          <li>
-            <Link href="/cart" className="block md:inline">
-              <div className="relative">
-                <Image
-                  src={CartIcon}
-                  alt="Cart Icon"
-                  className="top-4 h-8 w-8"
-                />
-                {cartItems > 0 && (
-                  <span className="absolute top-4 left-4 bg-red-500 text-white rounded-full p-0.5 text-xs">
-                    {cartItems}
-                  </span>
-                )}
-              </div>
-            </Link>
-          </li>
-        </ul>
-        <button
-          className="block md:hidden text-white focus:outline-none"
-          onClick={toggleMenu}
-        >
-          {menuOpen ? (
-            <Image src={NavbarIcon} alt="Close Icon" className="h-7 w-7" />
-          ) : (
-            <Image src={NavbarIcon} alt="Menu Icon" className="h-7 w-7" />
-          )}
-        </button>
-        {!isIndexPage && (
-          <div
-            className={`absolute ${
-              menuOpen ? "top-32 md:top-20" : "top-20"
-            } left-8`}
+        <div className="flex items-center space-x-4">
+          <ul
+            className={`${
+              menuOpen ? "block" : "hidden"
+            } md:flex md:space-x-4 md:justify-center mt-4 md:mt-0`}
           >
-            <button onClick={handleGoBack}>
+            <li>
+              <Link href="/" className="block md:inline">
+                <Image
+                  src={HomeIcon}
+                  alt="Home Icon"
+                  className="h-7 w-7 bottom-10"
+                />
+              </Link>
+            </li>
+            <li>
+              <Link href="/cart" className="block md:inline">
+                <div className="relative">
+                  <Image
+                    src={CartIcon}
+                    alt="Cart Icon"
+                    className="top-4 h-8 w-8"
+                  />
+                  {cartItems > 0 && (
+                    <span className="absolute top-4 left-4 bg-red-500 text-white rounded-full p-0.5 text-xs">
+                      {cartItems}
+                    </span>
+                  )}
+                </div>
+              </Link>
+            </li>
+          </ul>
+          <button
+            className="block md:hidden text-white focus:outline-none"
+            onClick={toggleMenu}
+          >
+            {menuOpen ? (
+              <Image src={NavbarIcon} alt="Close Icon" className="h-7 w-7" />
+            ) : (
+              <Image src={NavbarIcon} alt="Menu Icon" className="h-7 w-7" />
+            )}
+          </button>
+
+          {!isIndexPage && (
+            <div
+              className={`absolute ${
+                menuOpen ? "top-32 md:top-20" : "top-20"
+              } left-8`}
+            >
+              <button onClick={handleGoBack}>
+                <div className="flex items-center space-x-1">
+                  <Image src={BackIcon} alt="Back Icon" className="h-6 w-6" />
+                  <span>Volver</span>
+                </div>
+              </button>
+            </div>
+          )}
+          <Link href="/login" className="block md:inline">
+            <button className="text-white focus:outline-none">
               <div className="flex items-center space-x-1">
-                <Image src={BackIcon} alt="Back Icon" className="h-6 w-6" />
-                <span>Volver</span>
+                <Image src={loginIcon} alt="Login Icon" className="h-6 w-6" />
+                <span>Login</span>
               </div>
             </button>
-          </div>
-        )}
+          </Link>
+        </div>
       </nav>
     </div>
   );
