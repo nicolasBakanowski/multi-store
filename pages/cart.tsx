b7fe9f7e769f3dfaa98f33614537295f33a996d9
gonzacabrera2506 <gonzacabrera2506@gmail.com>
2023-09-26 22:46:42 -0300

    ADD: buttons eliminate all - eliminate one

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -2,22 +2,30 @@ import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "@/redux/store";
 import Image from "next/image";
-import { removeItem, discountProduct } from "@/redux/slices/cartSlice";
+import { removeItem, discountProduct, clearCart } from "@/redux/slices/cartSlice";
 import { CartItem } from "../interfaces/Cart";
 const CartPage: React.FC = () => {
   const dispatch = useDispatch();
   const cart = useSelector((state: RootState) => state.cart);
 
   const [productToRemove, setProductToRemove] = useState<CartItem | null>(null);
+  const [confirmRemoveAll, setConfirmRemoveAll] = useState(false);
+
 
   const handleRemoveFromCart = (product: CartItem) => {
     if (product.quantity > 1) {
       dispatch(discountProduct(product.id));
     } else {
-      dispatch(removeItem(product.id));
+      setProductToRemove(product);
+      setConfirmRemoveAll(true);
     }
     setProductToRemove(null);
   };
+
+  const handleRemoveAllQantity = (product: CartItem) => {
+    setProductToRemove(product);
+    setConfirmRemoveAll(true);
+  }
 
   const handleConfirmRemove = () => {
     if (productToRemove) {
@@ -25,8 +33,21 @@ const CartPage: React.FC = () => {
     }
   };
 
+  const handleConfirmRemoveAll = () => {
+    if (productToRemove) {
+      dispatch(removeItem(productToRemove.id));
+      setProductToRemove(null);
+      setConfirmRemoveAll(false);      
+    }
+  };
+
+  const handleCancelRemoveAll = () => {
+    setConfirmRemoveAll(false);
+    setProductToRemove(null);
+  };
+
   return (
-    <div className="bg-gray-100 min-h-screen">
+    <div className="bg-gray-100 min-h-screen text-center">
       <div className="container p-3">
         <h1 className="text-2xl font-semibold mb-4">Carrito de Compras</h1>
         {cart.length === 0 ? (
@@ -52,9 +73,15 @@ const CartPage: React.FC = () => {
                 </p>
                 <button
                   onClick={() => setProductToRemove(product)}
-                  className="bg-red-500 text-white px-3 py-1 rounded-md mt-2 hover:bg-red-600 focus:outline-none"
+                  className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900"
                 >
                   Quitar del Carro
+                </button>
+                <button
+                  onClick={() => handleRemoveAllQantity(product)}
+                  className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                >
+                  Eliminar producto
                 </button>
               </li>
             ))}
@@ -77,6 +104,31 @@ const CartPage: React.FC = () => {
                 </button>
                 <button
                   onClick={handleConfirmRemove}
+                  className="bg-red-500 text-white px-3 py-1 rounded-md hover:bg-red-600 focus:outline-none"
+                >
+                  Confirmar
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+        {/* Modal para la confirmación de eliminación de todas las unidades */}
+        {confirmRemoveAll && productToRemove && (
+          <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50">
+            <div className="bg-white p-4 rounded-md">
+              <p>
+                ¿Estás seguro de que deseas quitar todas las unidades de{" "}
+                {productToRemove.name} del carrito?
+              </p>
+              <div className="flex justify-end mt-4">
+                <button
+                  onClick={handleCancelRemoveAll}
+                  className="bg-gray-300 text-black px-3 py-1 rounded-md mr-2 hover:bg-gray-400 focus:outline-none"
+                >
+                  Cancelar
+                </button>
+                <button
+                  onClick={handleConfirmRemoveAll}
                   className="bg-red-500 text-white px-3 py-1 rounded-md hover:bg-red-600 focus:outline-none"
                 >
                   Confirmar
