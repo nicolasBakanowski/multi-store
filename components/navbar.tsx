ec343d2e9d33dea8cf9c4d708b75d03956933239
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-10-03 10:53:22 -0300

    feat: add sidebar menu

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,80 +1,53 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import NavbarIcon from "../public/hamburger.svg";
-import HomeIcon from "../public/home.svg";
 import BackIcon from "../public/back.svg";
-import LoginIcon from "../public/login.svg";
-import AdminIcon from "../public/login.svg"; // Reemplaza por el ícono correcto si lo tienes
+import AdminIcon from "../public/login.svg";
 import CartLink from "./CartLink";
+
 const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const cartItems = useSelector((state: RootState) => state.cart.length);
   const userRole = useSelector((state: RootState) => state.user.user?.roleId);
   const userName = useSelector((state: RootState) => state.user.user?.name);
+  const cartItems = useSelector((state: RootState) => state.cart.length);
+
+  const [menuOpen, setMenuOpen] = useState(false); // Estado para controlar el menú lateral
+  const router = useRouter();
+  const isIndexPage = router.pathname === "/";
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
-  const closeMenuOnResize = () => {
-    if (window.innerWidth >= 768) {
-      setMenuOpen(false);
-    }
+  const openMenu = () => {
+    setMenuOpen(true);
   };
-
-  const handleGoBack = () => {
-    router.back();
-  };
-
-  useEffect(() => {
-    window.addEventListener("resize", closeMenuOnResize);
-    return () => {
-      window.removeEventListener("resize", closeMenuOnResize);
-    };
-  }, []);
-
-  const router = useRouter();
-  const isIndexPage = router.pathname === "/";
 
   return (
-    <nav className="bg-gray-900 text-white py-4 px-6 flex justify-between items-center">
-      {!isIndexPage && (
-        <button onClick={handleGoBack}>
-          <div className="flex items-center space-x-1">
+    <nav className="bg-gray-900 text-white py-4 px-6 flex justify-between items-center relative">
+      {/* Fondo oscuro */}
+      {menuOpen && (
+        <div className="fixed inset-0 bg-black opacity-50 z-40"></div>
+      )}
+
+      {!isIndexPage ? (
+        <button onClick={() => router.back()}>
+          <div className="flex items-center">
             <Image src={BackIcon} alt="Back Icon" className="h-6 w-6" />
             <span>Volver</span>
           </div>
+        </button>
+      ) : (
+        <button>
+          <div className="flex items-center px-9"></div>
         </button>
       )}
       <div className="text-2xl font-bold flex w-1/3 justify-center">
         <Link href="/">Market</Link>
       </div>
       <div className="flex items-center space-x-4">
-        <ul
-          className={`${menuOpen ? "block" : "hidden"
-            } md:flex md:space-x-4 md:justify-center mt-4 md:mt-0`}
-        >
-          <li>
-            <Link href="/" className="block md:inline">
-              <Image src={HomeIcon} alt="Home Icon" className="h-7 w-7" />
-            </Link>
-          </li>
-        </ul>
-        <button
-          className="block md:hidden text-white focus:outline-none"
-          onClick={toggleMenu}
-        >
-          <Image
-            src={NavbarIcon}
-            alt={menuOpen ? "Close Icon" : "Menu Icon"}
-            className="h-7 w-7"
-          />
-        </button>
-        
         {userRole === 1 && (
           <Link href="/admin" className="block md:inline">
             <button className="text-white focus:outline-none">
@@ -90,30 +63,49 @@ const Navbar = () => {
           </Link>
         )}
         {userName ? (
-          <Link href="/login" className="block md:inline">
-            <button className="text-white focus:outline-none">
-              <div className="flex items-center space-x-1">
+          <div className="relative inline-block text-left z-50">
+            <button
+              className="text-white focus:outline-none"
+              onClick={toggleMenu}
+              onBlur={() => setMenuOpen(false)}
+            >
+              <div className="flex items-center space-x-1" onClick={openMenu}>
                 <span>{userName}</span>
               </div>
             </button>
-          </Link>
+            {/* Menú lateral */}
+            <div
+              className={`${
+                menuOpen ? "translate-x-0" : "translate-x-full"
+              } fixed top-0 right-0 h-full w-64 bg-white shadow-lg p-4 transform transition-transform ease-in-out duration-300 z-50`}
+            >
+              <a
+                href="#"
+                className="block py-2 text-gray-700 hover:bg-gray-100"
+              >
+                Configuraciones
+              </a>
+              <a
+                href="#"
+                className="block py-2 text-gray-700 hover:bg-gray-100"
+              >
+                Cerrar sesión
+              </a>
+            </div>
+          </div>
         ) : (
           <Link href="/login" className="block md:inline">
             <button className="text-white focus:outline-none">
               <div className="flex items-center space-x-1">
-                <Image src={LoginIcon} alt="Login Icon" className="h-6 w-6" />
                 <span>Login</span>
               </div>
             </button>
           </Link>
-        )
-        }
-
+        )}
       </div>
     </nav>
   );
 };
-
 const NavbarWithCartButton = () => {
   const cartItems = useSelector((state: RootState) => state.cart.length);
   const router = useRouter();
