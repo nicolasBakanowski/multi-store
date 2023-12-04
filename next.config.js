652bb84e246f499435be1a597135fa7749092093
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-12-04 17:34:46 -0300

    try fix cors problems

--- next.config.js
+++ next.config.js
@@ -2,7 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost", "192.168.0.46", "18.218.116.205", "localhost:30001"], // Agrega el dominio "localhost" aquí
+    domains: [
+      "localhost",
+      "192.168.0.46",
+      "18.218.116.205",
+      "https://multistore.3utilities.com",
+      "localhost:30001",
+    ], // Agrega el dominio "localhost" aquí
   },
 };
 
--- socket/socketConfig.ts
+++ socket/socketConfig.ts
@@ -4,7 +4,7 @@ import { io, Socket } from "socket.io-client";
 
 // Configura la URL del servidor Socket.IO
 const socketURL =
-  process.env.NEXT_PUBLIC_SOCKET_URL || "http://18.218.116.205/";
+  process.env.NEXT_PUBLIC_SOCKET_URL || "https://multistore.3utilities.com";
 
 // Crea e inicializa la instancia de Socket.IO
 const socket: Socket = io(socketURL);
