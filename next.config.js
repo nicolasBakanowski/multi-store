dd9ddfb8d2079891baf9c5472940bbd608133e8f
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-12-04 23:21:44 -0300

    add server ip in conf

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const nextConfig = {
     domains: [
       "localhost",
       "192.168.0.46",
+      "18.218.116.205",
+      "3.139.104.102",
       "https://multistore.3utilities.com",
       "localhost:30001",
     ], // Agrega el dominio "localhost" aquí
