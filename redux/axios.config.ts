ddeb0b2879bcbf14b12882ab318c39a83bf64c71
Nicolas Bakanowski <nicolasbakanowski@gmail.com>
2024-02-27 01:29:30 -0300

    refact: automatized env

--- redux/axios.config.ts
+++ redux/axios.config.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const instance: AxiosInstance = axios.create({
-  baseURL: "https://multistore.3utilities.com", // Reemplaza con la URL base de tu servidor API
+  baseURL: process.env.NEXT_PUBLIC_API_URL, // Reemplaza con la URL base de tu servidor API
 });
 
 export default instance;
