1d125c1d6d980496c77eeb7268865a43a464769c
Nicolas Bakanowski <nicolasbakanowski@gmail.com>
2024-04-23 18:33:59 -0300

    delete margin to spinner

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -1,6 +1,6 @@
 const Spinner = () => {
     return (
-        <div className="flex justify-center items-center mt-52">
+        <div className="flex justify-center items-center">
             <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-violet-950"></div>
         </div>
     );
