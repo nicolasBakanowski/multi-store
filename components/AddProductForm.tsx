4281c26b6bf5894be3d9ba55ae563314c8b4ee7b
Nicolas Bakanowski <nicolasbakanowski@gmail.com>
2024-10-21 19:57:05 -0300

    feat: add cost price at product

--- components/AddProductForm.tsx
+++ components/AddProductForm.tsx
@@ -14,26 +14,20 @@ const AddProductForm: React.FC = () => {
   const [productDescription, setProductDescription] = useState("");
   const [productStock, setProductStock] = useState("");
   const [productPrice, setProductPrice] = useState("");
+  const [costPrice, setCostPrice] = useState(""); // Nuevo estado para costPrice
   const [selectedImage, setSelectedImage] = useState<File | null>(null);
-  const [selectedCategory, setSelectedCategory] = useState<number | undefined>(
-    0
-  );
+  const [selectedCategory, setSelectedCategory] = useState<number | undefined>(0);
   const [isImageProcessing, setIsImageProcessing] = useState(false);
 
   const dispatch = useDispatch<AppDispatch>();
-  const categories = useSelector(
-    (state: RootState) => state.category.categories
-  );
+  const categories = useSelector((state: RootState) => state.category.categories);
   const router = useRouter();
   const userToken = useSelector((state: RootState) => state.user.token);
-  const isLoading = useSelector((state: RootState) => state.loading.isLoading)
-
+  const isLoading = useSelector((state: RootState) => state.loading.isLoading);
 
   useEffect(() => {
     dispatch(fetchCategories());
   }, []);
-
-
 
   const handleImageChange = async (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files.length > 0) {
@@ -42,12 +36,12 @@ const AddProductForm: React.FC = () => {
       webPImage ? setSelectedImage(webPImage) : dispatch(setNotification({ message: "No se pudo comprimir la imagen", type: "error" }));
       setIsImageProcessing(false);
     }
-  }
-
+  };
 
   const handleCategoryChange = (e: ChangeEvent<HTMLSelectElement>) => {
     setSelectedCategory(parseInt(e.target.value, 10));
   };
+
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
@@ -59,26 +53,28 @@ const AddProductForm: React.FC = () => {
       console.warn("Por favor, seleccione una categoría válida.");
       return;
     }
+
     const formData = new FormData();
     formData.append("name", productName);
     formData.append("description", productDescription);
     formData.append("stock", productStock);
     formData.append("price", productPrice);
+    formData.append("costPrice", costPrice); // Agregar costPrice al FormData
 
-    formData.append(
-      "categoryId",
-      selectedCategory !== undefined ? selectedCategory.toString() : ""
-    );
+    formData.append("categoryId", selectedCategory !== undefined ? selectedCategory.toString() : "");
 
     if (selectedImage) {
       formData.append("productImage", selectedImage);
     }
 
     try {
-      await dispatch(addProduct({ productData: formData, token: userToken })); setProductName("");
+      await dispatch(addProduct({ productData: formData, token: userToken }));
+      // Resetear los campos después de agregar el producto
+      setProductName("");
       setProductDescription("");
       setProductStock("");
       setProductPrice("");
+      setCostPrice(""); // Resetear costPrice
       setSelectedImage(null);
       setSelectedCategory(undefined);
     } catch (error) {
@@ -108,7 +104,6 @@ const AddProductForm: React.FC = () => {
               }}
               className="text-base p-2 border border-gray-300 rounded-lg w-full focus:outline-none focus:border-indigo-500"
               placeholder="Nombre del Producto"
-
               maxLength={40}
             />
             <p className="text-sm text-gray-500">{`${charCount}/40 caracteres`}</p>
@@ -140,7 +135,19 @@ const AddProductForm: React.FC = () => {
               placeholder="Stock del Producto"
             />
           </div>
-
+          {/* Cost Price */}
+          <div className="space-y-2">
+            <label className="text-sm font-bold text-gray-500 tracking-wide">
+              Precio de Costo del Producto:
+            </label>
+            <input
+              type="text"
+              value={costPrice}
+              onChange={(e) => setCostPrice(e.target.value)}
+              className="text-base p-2 border border-gray-300 rounded-lg w-full focus:outline-none focus:border-indigo-500"
+              placeholder="Precio de Costo del Producto"
+            />
+          </div>
           {/* Product Price */}
           <div className="space-y-2">
             <label className="text-sm font-bold text-gray-500 tracking-wide">
@@ -154,6 +161,8 @@ const AddProductForm: React.FC = () => {
               placeholder="Precio del Producto"
             />
           </div>
+
+
 
           {/* Category Selection */}
           <div className="space-y-2">
@@ -198,14 +207,12 @@ const AddProductForm: React.FC = () => {
             className="w-full bg-blue-500 text-gray-100 p-4 rounded-full tracking-wide
                       font-semibold focus:outline-none focus:shadow-outline hover:bg-blue-600 shadow-lg cursor-pointer transition ease-in duration-300"
           >
-            {isLoading || isImageProcessing ? <Spinner /> : "Guardar Categoría"}
+            {isLoading || isImageProcessing ? <Spinner /> : "Guardar Producto"}
           </button>
         </form>
       </div>
     </section>
   );
 };
-
-
 
 export default AddProductForm;
