b54e9a07fc2dbbd9cf46970dd72ed84bdecb69ab
Nicolas Bakanowski <nicolasbakanowski@gmail.com>
2024-10-28 18:27:16 -0300

    clean code for credencial

--- firebase.ts
+++ firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBUnRUWJnkAGWKyXZqMpsh20PrnixvGg5o",
-  authDomain: "pintabien-f205f.firebaseapp.com",
-  projectId: "pintabien-f205f",
-  storageBucket: "pintabien-f205f.appspot.com",
-  messagingSenderId: "542992009078",
-  appId: "1:542992009078:web:087a3d7c47611ac8bf9c57",
-  measurementId: "G-RKSPCVCFLL"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
